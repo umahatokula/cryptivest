8e1deb009edbccf22d7d5e4f8074b483b4ceec8d
Umaha Tokula <umahatokula@gmail.com>
2022-11-21 14:17:41 +0100

    First commit
    First commit with version 1 of the smart contract

---
+++ hardhat.config.js
@@ -0,0 +1,11 @@
+require('@nomiclabs/hardhat-waffle');
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.0"
+  },
+  paths: {
+    artifacts: "./client/src/artifacts",
+  }
+};
